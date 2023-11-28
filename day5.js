2877fcb47c55d26776e478bf7a2e534e412e6ec8
marie <ingabiremarieaimee6@gmail.com>
2023-11-28 11:41:57 -0800

    push

---
+++ day5.js
@@ -0,0 +1,94 @@
+console.log("let's  begin:");  
+console.log("Hello");  
+console.log("World");
+console.log("and  again:"); 
+ console.log("aimee"); 
+ console.log("World"); 
+ console.log("Hello"); 
+ console.log("World"); 
+
+ function  test()  {
+ }
+ console.log("aimee");
+ console.log("marie");
+
+
+ let counter = 100;
+ console.log(counter); 
+
+ {let counter = 200;
+console.log(counter);
+ } 
+ console.log(counter);
+
+
+ let me = 100;
+ console.log(me); 
+
+ { me = 200;
+console.log(me);
+ } 
+ console.log(me);
+
+ let Rose =70;
+
+  Rose =560/70*8
+ 
+ console.log("Rose")
+ 
+ let lily=50;
+ 
+lily =500/50*10
+ 
+ console.log("lily")
+ 
+ let tulip =120;
+ 
+ tulip=240/120*240
+ 
+ console.log("tulip")
+
+ price_per_rose = 8
+ num_roses = 70
+ 
+ price_per_lily = 10
+ num_lilies = 50
+ 
+ price_per_tulip = 2
+ num_tulips = 120
+ 
+ var contact1=
+ {
+ Name:"Maxwell Wright",
+ Phone:"(0191) 719 6495",
+ Email:"Curabitur.egestas.nunc@nonummyac.co.uk"
+ 
+ }
+ var contact2=
+ {
+     Name:"Raja Villarreal",
+     Phone:"0866 398 2895",
+     Email:"posuere.vulputate@sed.com"
+     
+ }
+ console.log(contact1)
+ var contact3=
+ {
+     Name:"Helen Richards",
+     Phone:"0800 1111",
+     Email:"libero@convallis.edu"
+ }
+ 
+ var contact={contact1,contact2,contact3};
+ //for first contact
+ console.log("contact1");
+ console.log("Name:"+contact1.Name);
+ console.log("phone:"+contact1.Phone);
+ console.log("Email:"+contact1.Email);
+ //for last contact
+ console.log("contact3");
+ console.log("Name:"+contact3.Name);
+ console.log("phone:"+contact3.Phone);
+ console.log("Email:"+contact3.Email);
+ 
+
